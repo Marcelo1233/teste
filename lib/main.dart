efcca2756b86d9753650c86d5785f6202c6bc50c
Marcelo1233 <marcelocrastok3@gmail.com>
2022-04-07 14:55:08 -0300

    j

---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Teste'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+          Container(
+            width: 200,
+            height: 200,
+            child: Image.asset('assets/imagens/img.png'),
+          ),
+          ],
+        ),
+      ), floatingActionButton: FloatingActionButton(
+          backgroundColor: Color.fromARGB(255, 101, 212, 174),
+          tooltip: 'Adicionar alimento',
+          child: const Icon(Icons.add),
+          onPressed: () {},
+        ),
+    );
+  }
+}
